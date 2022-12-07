638c999a8f28f504e9d40b2cc0817758c08c9fba
shota.miyahira <shota.miyahira@stafes.com>
2022-12-07 19:50:38 +0900

    rivを読み込み

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRive } from "@rive-app/react-canvas";
 
 function App() {
+  const { RiveComponent } = useRive({
+    src: "dog.riv",
+    autoplay: true,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "500px", height: "500px" }}>
+      <RiveComponent />
     </div>
   );
 }
